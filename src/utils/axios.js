1149ba5ea18d5726399f820edc934f42e64558ed
Sergey Karinsky <karinsky.dev@gmail.com>
2024-09-05 00:46:04 +0700

    Use api from env variables

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,7 +4,7 @@ import { getFromLocalStorage } from 'utils/common'
 import { STORAGE_KEY_USER_HASH, STORAGE_KEY_USER_TOKEN } from 'const'
 
 // export const API_HOST = 'https://ibronevik.ru/taxi'
-export const API_HOST = 'https://uticket.uventy.com/uventy'
+export const API_HOST = process.env.REACT_APP_API_URL
 export const API_URI = '/c/TikShow/api/v1/'
 export const API_URL = `${API_HOST}${API_URI}`
 export const axios = axiosHttp.create({
