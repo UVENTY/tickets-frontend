f625acab00133e2a4a524b53d1170cee0ef00b3d
Пользователь <ryabchenkora72@gmail.com>
2025-09-12 12:45:03 +0400

    do-not-show-percent

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -380,7 +380,7 @@ const CartModal = ({
               })}
             </div> */}
             <p className="w100 df aic jcsb" style={{ color: "#f8f5ec80" }}>
-              <span className="fs12">service fee {fee}%:</span>
+              <span className="fs12">service fee<span style={{ display: "none" }}>{fee}%</span>:</span>
               <i className="fs12">
                 <b>{t?.fee || 0} €</b>
               </i>
